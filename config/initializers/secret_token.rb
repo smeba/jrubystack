698260b4072eb94ce06e64db916f80f0645a3036
smeba <francois.painlong@laposte.net>
2012-07-16 03:00:18 +0200

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Jrubystack::Application.config.secret_token = 'f1496469c82fcac622e95c3fd91686866547d7a34726be57a43daa2c42bc02835863ee7956a24fde5a58f758e084e3f76c8c73e5c251bbf40ebc86684c6e8b02'
